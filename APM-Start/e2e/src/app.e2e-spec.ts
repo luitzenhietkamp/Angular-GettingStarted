de2490b7d43c654b5f85a5de4b74ceb56d9e2f5b
Luitzen Hietkamp <luitzenhietkamp@gmail.com>
2020-04-13 10:09:07 +0100

    Fix E2E tests

--- APM-Start/e2e/src/app.e2e-spec.ts
+++ APM-Start/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to APM!');
+    expect(page.getTitleText()).toEqual('ACME Product Management');
   });
 
   afterEach(async () => {
